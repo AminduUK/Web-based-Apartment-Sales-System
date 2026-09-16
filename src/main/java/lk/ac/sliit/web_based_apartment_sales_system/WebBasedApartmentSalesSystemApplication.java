04783f87ec1d3ee45fbc75d01c17a27cab1369ce
AminduUK <aminduumayangakule23@gmail.com>
2026-09-16 10:42:17 +0530

    Initial commmit

---
+++ src/main/java/lk/ac/sliit/web_based_apartment_sales_system/WebBasedApartmentSalesSystemApplication.java
@@ -0,0 +1,13 @@
+package lk.ac.sliit.web_based_apartment_sales_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBasedApartmentSalesSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBasedApartmentSalesSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ac/sliit/web_based_apartment_sales_system/WebBasedApartmentSalesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.sliit.web_based_apartment_sales_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBasedApartmentSalesSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
